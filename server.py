2a4f85011e0175e5b7064c42d610aa37444ee088
Jaeyeon Kim <0310kjy@gmail.com>
2022-12-10 19:32:50 +0900

    [Feat] Load model once

--- server.py
+++ server.py
@@ -8,11 +8,53 @@ from models.ColorVidNet import ColorVidNet
 from colorize_img import colorize_image
 
 app = Flask(__name__)
+first = True
+
+
+def model_load():
+    # Set arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--frame_propagate", default=False, type=bool)
+    parser.add_argument("--image_size", type=int, default=[216 * 2, 384 * 2])
+    parser.add_argument("--cuda", action="store_false")
+    parser.add_argument("--gpu_ids", type=str, default="0")
+    parser.add_argument("--clip_path", type=str, default="./images/input")
+    parser.add_argument("--ref_path", type=str, default="./images/ref")
+    parser.add_argument("--output_path", type=str, default="./images/output")
+    opt = parser.parse_args()
+
+    # Load networks & models
+    print("Loading models ...")
+    nonlocal_net = WarpNet(1)
+    colornet = ColorVidNet(7)
+    vggnet = VGG19_pytorch()
+    vggnet.load_state_dict(torch.load("data/vgg19_conv.pth"))
+
+    for param in vggnet.parameters():
+        param.requires_grad = False
+
+    nonlocal_test_path = os.path.join("checkpoints/", "video_moredata_l1/nonlocal_net_iter_76000.pth")
+    color_test_path = os.path.join("checkpoints/", "video_moredata_l1/colornet_iter_76000.pth")
+
+    nonlocal_net.load_state_dict(torch.load(nonlocal_test_path, map_location=torch.device('cpu')))
+    colornet.load_state_dict(torch.load(color_test_path, map_location=torch.device('cpu')))
+
+    nonlocal_net.eval()
+    colornet.eval()
+    vggnet.eval()
+    nonlocal_net.cpu()
+    colornet.cpu()
+    vggnet.cpu()
+    print("Model loaded")
+    return opt, nonlocal_net, colornet, vggnet
 
 
 @app.route('/colorization', methods=['POST'])
 def img_save():
-
+    global opt, nonlocal_net, colornet, vggnet, first
+    if first:
+        opt, nonlocal_net, colornet, vggnet = model_load()
+        first = False
     # Get input image
     image_file = request.files.get('input', '')
     filename = secure_filename(image_file.filename)
@@ -59,41 +101,5 @@ def img_save():
 
 
 if __name__ == "__main__":
-    # Set arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--frame_propagate", default=False, type=bool)
-    parser.add_argument("--image_size", type=int, default=[216 * 2, 384 * 2])
-    parser.add_argument("--cuda", action="store_false")
-    parser.add_argument("--gpu_ids", type=str, default="0")
-    parser.add_argument("--clip_path", type=str, default="./images/input")
-    parser.add_argument("--ref_path", type=str, default="./images/ref")
-    parser.add_argument("--output_path", type=str, default="./images/output")
-    opt = parser.parse_args()
-
-    # Load networks & models
-    print("Loading models ...")
-    nonlocal_net = WarpNet(1)
-    colornet = ColorVidNet(7)
-    vggnet = VGG19_pytorch()
-    vggnet.load_state_dict(torch.load("data/vgg19_conv.pth"))
-
-    for param in vggnet.parameters():
-        param.requires_grad = False
-
-    nonlocal_test_path = os.path.join("checkpoints/", "video_moredata_l1/nonlocal_net_iter_76000.pth")
-    color_test_path = os.path.join("checkpoints/", "video_moredata_l1/colornet_iter_76000.pth")
-
-    nonlocal_net.load_state_dict(torch.load(nonlocal_test_path, map_location=torch.device('cpu')))
-    colornet.load_state_dict(torch.load(color_test_path, map_location=torch.device('cpu')))
-
-    nonlocal_net.eval()
-    colornet.eval()
-    vggnet.eval()
-    nonlocal_net.cpu()
-    colornet.cpu()
-    vggnet.cpu()
-    print("Model loaded")
-
     # Start app
-    print("Start server !")
     app.run()
